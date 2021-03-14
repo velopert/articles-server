367309e1451df99584f210c8c8c69ede27b636e8
velopert <public.velopert@gmail.com>
2021-03-15 03:54:26 +0900

    Finalized server

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -1,8 +1,61 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  async create(ctx) {
+    // 사용자의 id를 데이터에 추가
+    ctx.request.body.user = ctx.state.user.id;
+    // article 데이터 생성
+    const entity = await strapi.services.article.create(ctx.request.body);
+    // 잘못된 필드 및 Private 값 제외하고 반환
+    return sanitizeEntity(entity, { model: strapi.models.article });
+  },
 
-module.exports = {};
+  async update(ctx) {
+    const { id } = ctx.params; // URL 파라미터에서 id 추출
+    const article = await strapi.services.article.findOne({ id }); // id 로 데이터 조회
+
+    // 데이터가 존재하지 않을 때
+    if (!article) {
+      return ctx.throw(404);
+    }
+
+    // user 정보는 변경할 수 없도록 처리
+    if (ctx.request.body.user) {
+      return ctx.throw(400, 'user field cannot be changed');
+    }
+
+    // 사용자의 id 와 article의 작성자 id가 일치하는지 확인
+    if (ctx.state.user.id !== article.user.id) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+    // article 데이터 업데이트
+    const entity = await strapi.services.article.update(
+      { id },
+      ctx.request.body
+    );
+
+    // 응답 반환
+    return sanitizeEntity(entity, { model: strapi.models.article });
+  },
+
+  async delete(ctx) {
+    const { id } = ctx.params; // URL 파라미터에서 id 추출
+    const article = await strapi.services.article.findOne({ id }); // id 로 데이터 조회
+
+    // 데이터가 존재하지 않을 때
+    if (!article) {
+      return ctx.throw(404);
+    }
+
+    // 사용자의 id 와 article의 작성자 id가 일치하는지 확인
+    if (ctx.state.user.id !== article.user.id) {
+      return ctx.unauthorized(`You can't remove this entry`);
+    }
+
+    // 데이터 삭제
+    await strapi.services.article.delete({ id });
+
+    // 응답 반환
+    ctx.status = 204; // no content
+  },
+};
--- api/comment/controllers/comment.js
+++ api/comment/controllers/comment.js
@@ -1,8 +1,82 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  async create(ctx) {
+    // 사용자의 id를 데이터에 추가
+    ctx.request.body.user = ctx.state.user.id;
+    const { articleId } = ctx.params;
+    ctx.request.body.article = articleId;
 
-module.exports = {};
+    // 게시글 존재 유무 확인
+    const article = await strapi.services.article.findOne({ id: articleId }); // id 로 데이터 조회
+    if (!article) {
+      ctx.throw(404);
+    }
+
+    // Comment 데이터 생성
+    const entity = await strapi.services.comment.create(ctx.request.body);
+    // 응답 반환
+    return sanitizeEntity(entity, { model: strapi.models.comment });
+  },
+  async find(ctx) {
+    // articleId 로 댓글 조회
+    const entities = await strapi.services.comment.find({
+      article: ctx.params.articleId,
+    });
+    // 각 데이터들에 대하여 sanitizeEntity를 처리하여 응답 반환
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.comment })
+    );
+  },
+  async update(ctx) {
+    const { articleId, id } = ctx.params; // URL 파라미터 추출
+    // 댓글 조회
+    const comment = await strapi.services.comment.findOne({
+      id,
+      article: articleId,
+    });
+    // 데이터가 존재하지 않을 때
+    if (!comment) {
+      return ctx.throw(404);
+    }
+    // article 또는 user 변경 막기
+    if (ctx.request.body.article || ctx.request.body.user) {
+      return ctx.throw(400, 'article or user field cannot be changed');
+    }
+    // 사용자 확인
+    if (ctx.state.user.id !== comment.user.id) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+    // comment 데이터 업데이트
+    const entity = await strapi.services.comment.update(
+      {
+        id,
+      },
+      ctx.request.body
+    );
+    // 응답 반환
+    return sanitizeEntity(entity, { model: strapi.models.comment });
+  },
+  async delete(ctx) {
+    const { articleId, id } = ctx.params; // URL 파라미터 추출
+    // 댓글 조회
+    const comment = await strapi.services.comment.findOne({
+      id,
+      article: articleId,
+    });
+    // 데이터가 존재하지 않을 때
+    if (!comment) {
+      return ctx.throw(404);
+    }
+
+    // 사용자 확인
+    if (ctx.state.user.id !== comment.user.id) {
+      return ctx.unauthorized(`You can't remove this entry`);
+    }
+
+    // 데이터 삭제
+    await strapi.services.comment.delete({ id });
+
+    ctx.status = 204;
+  },
+};
