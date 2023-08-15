1f47bcf7d78bee728e576b4d3fa87f9d12888a4f
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-08-16 03:04:55 +0900

    Migrate strapi v3 to v4
    - Node 14 버전에만 지원하는 strapi v3 대신 v4를 사용하도록 migration

--- v3/config/server.js
+++ v3/config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c49939c2901c4cf1395e00e5ec470a2b'),
+    },
   },
 });
